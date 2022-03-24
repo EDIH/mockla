5c18dab4a60a0e25a96145988c922dadb1ce4dca
Сергей <smolanovsergey@gmail.com>
2022-03-24 17:50:49 +0200

    2 section

---
+++ resources/views/client/block_templates/templates/critic.blade.php
@@ -0,0 +1,58 @@
+<?php
+$contents = $block->mappedByKey();
+?>
+<section class="critic">
+    <div class="critic__container main-container">
+        <div class="critic__image">
+            <img src="{{url('/'). '/img/templates/critic/img.png'}}" alt="sdfsdf">
+        </div>
+        <div class="critic__about">
+            <h2 class="critic__title section-title">
+                Be a Critic, be <br>
+                a Producer, be a Leader.<br>
+                Earn wealth on<br>
+                digital art!
+            </h2>
+            <div class="critic__desc">
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M39.6693 20.0027C28.885 20.1775 20.1757 28.8879 20.0026 39.6726L20 40L19.9973 39.6693C19.8225 28.885 11.1121 20.1757 0.327436 20.0026L0 20L0.330737 19.9973C11.2239 19.8207 20 10.9352 20 0C20 10.9352 28.7761 19.8207 39.6693 19.9973L40 20L39.6693 20.0027Z" fill="#ED6A32"/>
+                </svg>
+                <h3 class="critic__unsubtitle section-unsubtitle">
+                    Yolllo Business
+                </h3>
+                <p class="section-subtitle">
+                    Make your way to the life you want!
+                </p>
+                <div class="critic__excerpt">
+                    Couse with us you'll become a pro in notime :)
+                </div>
+            </div>
+        </div>
+        <div class="critic__list">
+            <div class="critic__item">
+                <div class="critic__icon">
+                    <img src="{{url('/'). '/img/templates/critic/icon1.svg'}}" alt="We'll guide you through every step!">
+                </div>
+                <h3 class="critic__description">
+                    We'll guide you through every step!
+                </h3>
+            </div>
+            <div class="critic__item">
+                <div class="critic__icon">
+                    <img src="{{url('/'). '/img/templates/critic/icon2.svg'}}" alt="We're lucky with you!">
+                </div>
+                <h3 class="critic__description">
+                    We're lucky with you!
+                </h3>
+            </div>
+            <div class="critic__item">
+                <div class="critic__icon">
+                    <img src="{{url('/'). '/img/templates/critic/icon3.svg'}}" alt="We'll give you all our exclusive apps to ease your routine ;)">
+                </div>
+                <h3 class="critic__description">
+                    We'll give you all our exclusive apps to ease your routine ;)
+                </h3>
+            </div>
+        </div>
+    </div>
+</section>
