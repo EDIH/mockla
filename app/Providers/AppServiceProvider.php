f0f14247fa29e1f1a44d499f5b57832d7d943904
666 <illia.averyanov@gmail.com>
2022-03-25 18:39:50 +0000

    [-] comments from menu builder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,24 +31,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(Dispatcher $events)
     {
 // add non-approved comments counter to sidebar menu
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $item = [
-                'text' => 'Комментарии',
-                'url' => 'admin/comment',
-                'icon' => 'far fa-fw fa-address-card',
-                'label_color' => 'danger',
-            ];
-
-            if($count = DB::table('comments')
-                ->where('is_approved', false)
-                ->where('deleted_at', null)
-                ->count()) {
-
-                $item['label'] =  $count;
-            }
-
-            $event->menu->add($item);
-        });
 
         $variables = Variable::all()
             ->groupBy('section');
