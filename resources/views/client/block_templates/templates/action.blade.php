c7a1b61465d334c71a76ad7e316048c18adbfdd4
Сергей <smolanovsergey@gmail.com>
2022-03-25 08:54:40 +0200

    вёрстка шаблонов

---
+++ resources/views/client/block_templates/templates/action.blade.php
@@ -0,0 +1,17 @@
+<section class="action">
+    <div class="action__container main-container">
+        <div class="action__wrapper main-container">
+            <div class="action__text">
+                <h2 class="action__title">
+                    Are you ready to take action?
+                </h2>
+                <a href="#" class="action__lnk pink-button">
+                    I am ready!
+                </a>
+            </div>
+            <div class="action__image">
+                <img src="{{ url('/') }}/img/templates/action/img-desktop.png">
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/client/block_templates/templates/education.blade.php
@@ -0,0 +1,24 @@
+<section class="education main-container">
+    <div class="education__container main-container">
+        <div class="education__image">
+            <img src="{{ url('/') }}/img/templates/education/img.png">
+        </div>
+        <div class="education__text">
+            <h2 class="education__title">
+                Vividly Education
+                Your lifetime
+                skill-booster
+            </h2>
+            <h3 class="education__subtitle">
+                Software, lectures, master classes, real practice, all of that is your to use for your success!
+            </h3>
+            <a href="#" class="education__lnk">
+                Details
+                <svg width="33" height="17" viewBox="0 0 33 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 8.5H30" stroke="#0D0E17" stroke-width="2.5"/>
+                    <path d="M24 1.5L31 8.5L24 15.5" stroke="#0D0E17" stroke-width="2.5"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+</section>
